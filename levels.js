f4d73649a696e5ad0602fb487cdc72cb2f548c34
Liran Markin <liran.markin@gmail.com>
2018-06-17 02:29:41 +0300

    changed ui, added examples and easy levels

--- levels.js
+++ levels.js
@@ -1,31 +1,57 @@
 
-const factorial = x => x < 2 ? 1 : x*factor(x-1)
+const factorial = (x) => x < 2 ? 1 : x * factorial(x - 1)
 
-const fib = x => x <= 2 ? 1 : fib(x-1) + fib(x-2)
+const fib = (x) => x <= 2 ? 1 : fib(x - 1) + fib(x - 2)
 
 const levels = [
 	{
-		func: x => x+1,
+		func: (x) => x + 1,
+		examples: [4, 6],
 		tests: [9, 15]
 	},
 	{
-		func: x => x*x,
+		func: (x) => x - 2,
+		examples: [10, 14],
+		tests: [6, 1]
+	},
+	{
+		func: (x) => 3 * x,
+		examples: [4, 9],
+		tests: [7, 12]
+	},
+	{
+		func: (x) => 2 * x - 3,
+		examples: [6, 8],
+		tests: [9, 11]
+	},
+	{
+		func: (x) => Math.abs(x),
+		examples: [0, -2],
+		tests: [9, -15]
+	},
+	{
+		func: (x) => x * x,
+		examples: [3, 6],
 		tests: [8, 10]
 	},
 	{
-		func: x => x*(x+1)/2,
+		func: (x) => x * (x + 1) / 2,
+		examples: [2, 4],
 		tests: [12]
 	},
 	{
-		func: x => Math.pow(2, x),
+		func: (x) => Math.pow(2, x),
+		examples: [3, 5],
 		tests: [6]
 	},
 	{
 		func: factorial,
+		examples: [5, 6],
 		tests: [7]
 	},
 	{
 		func: fib,
+		examples: [3, 4, 5],
 		tests: [11]
 	},
 ]
--- main.js
+++ main.js
@@ -1,56 +1,89 @@
 
 const number_prompt = message => parseFloat( prompt(message) )
 
-const ask = input => number_prompt("what is the answer for the input:\n" + input + "\n?")
+const ask = (input) => number_prompt(`Input = ${input}\nOutput = `)
 
-const test_level = level => {
+const test_level = (level) => {
 	const func = level.func
 
-	for(const input of level.tests)
-	if(func(input) != ask(input))
+	for (const input of level.tests)
+	if (func(input) != ask(input))
 		return false
 
 	return true
 }
-
-const query_level = level => {
-	const input = number_prompt("what is your input?")
-	if(isNaN(input)) 					 alert("Input must be a number!")
-	else if(level.tests.includes(input)) alert("Can't ask it")
-	else 								 alert("Result = " + level.func(input))
+const write_message = (message) => {
+	const message_element =
+		`<div class="row alert alert-primary" role="alert">
+			<h2>${message}</h2>
+		</div>`
+	$("#message-list").prepend(message_element)
 }
 
-const setLevelHeader = level_num => document.getElementById("level").innerHTML = "Level: " + (level_num)
+const clear_message = () => $("#message-list").empty()
 
-let cur_level = localStorage.getItem("cur_level") || 0;
-setLevelHeader(cur_level)
+const query_level = (level) => {
+	const input = parseFloat($("#input").val())
+	if (isNaN(input)) 					  write_message("Input must be a number!")
+	else if (level.tests.includes(input)) write_message("Can't ask it, it is in the test!")
+	else 								  write_message(`Input = ${input} | Output = ${level.func(input)}`)
+}
 
+const show_examples = (level) => {
+	const func = level.func
+	for (const input of level.examples)
+		write_message(`Input = ${input} | Output = ${level.func(input)}`)
+}
 
-const check_level = level_num => {
-	if(level_num >= levels.length){
-		alert("Reached highest level!")
+const set_level_header = (level_ind) => $("#level").html(`Level: ${level_ind}`)
+
+const set_level = (new_level, callback = () => {}) => {
+	level_ind = new_level
+	set_level_header(new_level)
+	localStorage.setItem("level", new_level)
+	clear_message()
+	callback()
+	if (check_level(level_ind))
+		show_examples(levels[level_ind])
+}
+
+const check_level = (level_ind) => {
+	if(level_ind >= levels.length){
+		write_message("Reached highest level!")
 		return false
 	}
 	return true
 }
 
 const test = () => {
-	if(!check_level(cur_level)) return
+	if (!check_level(level_ind)) return
 
-	if(test_level(levels[cur_level])){
-		alert("You passed! :)")
-		setLevelHeader(++cur_level)
-		localStorage.setItem("cur_level", cur_level)
+	if (test_level(levels[level_ind])){
+		set_level(level_ind + 1, () => write_message("You passed the test! :)"))
+	} else {
+		write_message("You failed the test :(")
 	}
-	else
-		alert("You failed :(")
 }
 
-const query = () => check_level(cur_level) && query_level(levels[cur_level])
-
-const set_level = level => {
-	localStorage.setItem("cur_level", level)
-	location.reload()
+const query = () => {
+	check_level(level_ind) && query_level(levels[level_ind])
+	$("#input").val("")
 }
 
-const reset = () => set_level(0)
+const reset = () => {
+	if (confirm("Are you sure you want to reset your progress?")){
+		localStorage.setItem("level", 0)
+		location.reload()
+	}
+}
+
+let level_ind = 0;
+$(document).ready(() => {
+	level_ind = parseFloat(localStorage.getItem("level")) || 0
+	set_level(level_ind)
+})
+
+$("#input").keyup((event) => {
+	if (13 == event.which)
+		query()
+})
